882b86a335affb1c0e016b2e24ba4fe21c7e6777
Rachel Johnson <rachel.johnson@ucdenver.edu>
2017-09-03 21:17:23 -0600

    Renamed file to keep code together. Created demographics table.

---
+++ Project0/Code/Project 0 Code.R
@@ -0,0 +1,165 @@
+#=================================================================#
+# Project 0
+# Purpose: Analyze data to determine if dental gel improves gum health measurements
+# Rachel Johnson
+# Code started: 08/30/2017
+# Last updated: 08/30/2017
+#=================================================================#
+
+#=================================================================#
+# Import and clean data
+#=================================================================#
+
+setwd("~/School/AdvancedData")
+gums <- read.csv("Project0_dental_data.csv", header = T)
+
+#=================================================================#
+# Explore all variables indiv. (even though randomized trial)
+#=================================================================#
+
+names(gums)
+
+table(gums$id) #no repeat IDs
+table(gums$trtgroup) #evenly spread; 26 in each group
+table(gums$trtgroup) #not an even number of men and women
+table(gums$sex, gums$trtgroup) #however there are approx. equal in each group
+table(gums$race) #definitely uneven numbers
+
+table(gums$age)
+gums$age <- as.numeric(gums$age)
+summary(gums$age)
+
+table(gums$smoker, gums$trtgroup)
+summary(gums$sites)
+
+summary(gums$attachbase)
+boxplot(gums$attachbase)
+
+summary(gums$attach1year)
+boxplot(gums$attach1year)
+
+gums$diffattach <- gums$attach1year - gums$attachbase
+summary(gums$diffattach)
+boxplot(gums$diffattach)
+
+summary(gums$pdbase)
+boxplot(gums$pdbase)
+
+summary(gums$pd1year)
+boxplot(gums$pd1year)
+
+gums$diffpd <- gums$pd1year - gums$pdbase
+summary(gums$diffpd)
+boxplot(gums$diffpd)
+
+#=================================================================#
+# Label variables from data dictionary
+#=================================================================#
+
+labels <- gums
+
+names(labels)
+
+table(labels$trtgroup)
+labels$trtgroup <- factor(labels$trtgroup, levels = c("1", "2", "3", "4", "5"),
+                          labels = c("Placebo", "Control", "Low Dose",
+                                     "Medium Dose", "High Dose"))
+
+table(labels$sex)
+labels$sex <- factor(labels$sex, levels = c("1", "2"), labels =
+                       c("Male", "Female"))
+
+table(labels$race)
+labels$race <- factor(labels$race, levels = c("1", "2", "4", "5"),
+                      labels = c("Native American", "African American",
+                                 "Asian", "White"))
+
+table(labels$smoker)
+labels$smoker <- factor(labels$smoker, levels = c("0", "1"), labels = c("No", "Yes"))
+
+
+#=================================================================#
+# Investigate missing data for outcome
+#=================================================================#
+
+missing <- gums[is.na(gums$pd1year) == TRUE, ]
+table(missing$sex)
+table(missing$race)
+summary(missing$age)
+table(missing$smoker)
+#None of these contain unusual patterns that would explain missingness
+
+#Since they're missing the outcomes, they will be excluded from the analysis
+  #therefore, will exclude them from dataset now
+labels <- labels[is.na(labels$pd1year) == FALSE, ]
+gums <- gums[is.na(gums$pd1year) == FALSE, ]
+
+#=================================================================#
+# Divide into group data sets to make table
+#=================================================================#
+
+levels(labels$trtgroup)
+group1 <- labels[labels$trtgroup == "Placebo", ]
+group2 <- labels[labels$trtgroup == "Control", ]  
+group3 <- labels[labels$trtgroup == "Low Dose", ]
+group4 <- labels[labels$trtgroup == "Medium Dose", ]  
+group5 <- labels[labels$trtgroup == "High Dose", ] 
+#Slightly more are missing from group 5, but not an incredible amt more 
+
+
+#=================================================================#
+# Divide into group data sets to make table
+#=================================================================#
+
+#Want to include sex (3 row), race (5 row), age (1), smoker (3), sites (1), 
+  #attach base (1), attach 1yr (1), pdbase (1), pd1 yr (1)
+num <- 3 + 5 + 1 + 3 + 1 + 1 + 1 + 1 + 1
+tab <- matrix(data = NA, ncol = 6, nrow = 17)
+colnames(tab) <- c("", levels(labels$trtgroup))
+tab[, 1] <- c("Sex (n (%))", levels(labels$sex), "Race (n (%))", levels(labels$race),
+              "Age (mean (sd))", "Smoker (n (%))", levels(labels$smoker), 
+              "Sites measured (mean (sd))", "Attachment at baseline (mean (sd))",
+              "Attachment at 1 year (mean (sd))", "Pocket depth at baseline (mean (sd))",
+              "Pocket depth at 1 year (mean (sd))")
+
+#NEED TO GO BACK AND CHANGE MEAN STATEMENTS
+for(i in 1:5){
+  tab[2, i+1] <- paste(nrow(gums[gums$trtgroup == i & gums$sex == 1, ]), "(",
+                     round(nrow(gums[gums$trtgroup == i & gums$sex == 1, ])/nrow(gums[gums$trtgroup == i, ]), 2), ")")
+  tab[3, i+1] <- paste(nrow(gums[gums$trtgroup == i & gums$sex == 2, ]), "(",
+                       round(nrow(gums[gums$trtgroup == i & gums$sex == 2, ])/nrow(gums[gums$trtgroup == i, ]), 2), ")")
+  tab[5, i+1] <- paste(nrow(gums[gums$trtgroup == i & gums$race == 1, ]), "(",
+                       round(nrow(gums[gums$trtgroup == i & gums$race == 1, ])/nrow(gums[gums$trtgroup == i, ]), 2), ")")
+  tab[6, i+1] <- paste(nrow(gums[gums$trtgroup == i & gums$race == 2, ]), "(",
+                       round(nrow(gums[gums$trtgroup == i & gums$race == 2, ])/nrow(gums[gums$trtgroup == i, ]), 2), ")")
+  tab[7, i+1] <- paste(nrow(gums[gums$trtgroup == i & gums$race == 4, ]), "(",
+                       round(nrow(gums[gums$trtgroup == i & gums$race == 4, ])/nrow(gums[gums$trtgroup == i, ]), 2), ")")
+  tab[8, i+1] <- paste(nrow(gums[gums$trtgroup == i & gums$race == 5, ]), "(",
+                       round(nrow(gums[gums$trtgroup == i & gums$race == 5, ])/nrow(gums[gums$trtgroup == i, ]), 2), ")")
+  tab[9, i+1] <- paste(round(mean(gums$age, na.rm = T), 2), "(", 
+                        round(sd(gums$age, na.rm = T), 2), ")")
+  tab[11, i+1] <- paste(nrow(gums[gums$trtgroup == i & gums$smoker == 0, ]), "(",
+                        round(nrow(gums[gums$trtgroup == i & gums$smoker == 0, ])/nrow(gums[gums$trtgroup == i, ]), 2), ")")
+  tab[12, i+1] <- paste(nrow(gums[gums$trtgroup == i & gums$smoker == 1, ]), "(",
+                        round(nrow(gums[gums$trtgroup == i & gums$smoker == 1, ])/nrow(gums[gums$trtgroup == i, ]), 2), ")")
+  tab[13, i+1] <- paste(round(mean(gums$sites, na.rm = T), 2), "(", 
+                       round(sd(gums$sites, na.rm = T), 2), ")")
+  tab[14, i+1] <- paste(round(mean(gums$attachbase, na.rm = T), 2), "(", 
+                        round(sd(gums$attachbase, na.rm = T), 2), ")")
+  tab[15, i+1] <- paste(round(mean(gums$attach1year, na.rm = T), 2), "(", 
+                        round(sd(gums$attach1year, na.rm = T), 2), ")")
+  tab[16, i+1] <- paste(round(mean(gums$pdbase, na.rm = T), 2), "(", 
+                        round(sd(gums$pdbase, na.rm = T), 2), ")")
+  tab[17, i+1] <- paste(round(mean(gums$pd1year, na.rm = T), 2), "(", 
+                        round(sd(gums$pd1year, na.rm = T), 2), ")")
+}
+
+setwd("C:/Repositories/bios6623-johnsra3/Project0/Processed")
+write.csv(tab, "DemographicsTable.csv")
+
+#=================================================================#
+# Explore univ relationships b/t covs & outcome 1: attach 1 year
+#=================================================================#
+
+
+
