094de30c9704cae1b544790617640dcf3f128e35
Rachel Johnson <rachel.johnson@ucdenver.edu>
2017-09-24 17:02:24 -0600

    Demographics table.

--- Project1/Code/Project 1 Code- Exploratory analysis.R
+++ Project1/Code/Project 1 Code- Exploratory analysis.R
@@ -22,6 +22,7 @@ hiv2 <- hiv_initial[hiv_initial$years == 2, ] #506 people at 2 yr mark
 # Clean data
 #=============================================================#
 
+##################
 #Remove people who don't have data at 2 years
 hiv0 <- hiv0[hiv0$newid %in% hiv2$newid, ]
 
@@ -29,6 +30,7 @@ hiv0 <- hiv0[, c(which(colnames(hiv0) == "newid"),
                  which(colnames(hiv0) == "AGG_MENT"),
                  which(colnames(hiv0) == "AGG_PHYS"),
                  which(colnames(hiv0) == "LEU3N"),
+                 which(colnames(hiv0) == "VLOAD"),
                  which(colnames(hiv0) == "hard_drugs"),
                  which(colnames(hiv0) == "age"),
                  which(colnames(hiv0) == "BMI"),
@@ -40,63 +42,213 @@ hiv0 <- hiv0[, c(which(colnames(hiv0) == "newid"),
                  which(colnames(hiv0) == "EDUCBAS"))] 
 
 hiv2 <- hiv2[, c(which(colnames(hiv2) == "newid"),
-                 which(colnames(hiv2) == "ADH"))]
+                 which(colnames(hiv2) == "ADH"),
+                 which(colnames(hiv2) == "AGG_MENT"),
+                 which(colnames(hiv2) == "AGG_PHYS"),
+                 which(colnames(hiv2) == "LEU3N"),
+                 which(colnames(hiv2) == "VLOAD"))]
 
-# 2. In previous analyses, we have adjusted for the following variables:
 
+###################
 #Need to categorize race, alcohol use, smoke, income, education, ART adherence
 #Race- change to NHW (non-hispanic white) vs. Other
 table(hiv0$RACE)
 hiv0$RACE_cat <- hiv0$RACE  
-hiv0$RACE_cat <- ifelse(hiv0$RACE_cat == 1, "Non-White Hispanic", "Other")
+hiv0$RACE_cat <- ifelse(hiv0$RACE_cat == 1, "Non-Hispanic White", "Other")
+table(hiv0$RACE_cat)
 
 #Alcohol
 table(hiv0$DKGRP)
 hiv0$drink_cat <- hiv0$DKGRP
 hiv0$drink_cat <- ifelse(hiv0$DKGRP == 3, "> 13 drinks per week", "13 or fewer drinks per week")
+table(hiv0$drink_cat)
 
 #Smoking
 table(hiv0$SMOKE)
 hiv0$smoke_cat <- hiv0$SMOKE
 hiv0$smoke_cat <- ifelse(hiv0$smoke_cat == 3, "Current", "Never/former")
+table(hiv0$smoke_cat)
 
 #Income 
 table(hiv0$income)
 hiv0$income_cat <- hiv0$income
-hiv0$income_cat <- if(hiv0$income_cat == 1){
-  hiv0$income_cat <- "< $10,000"
-} else {
-  if(hiv$income_cat)
-}
-#figure out how to categorize this :) 
+hiv0$income_cat[hiv0$income_cat == 9] <- NA #remove those who chose not to answer
+hiv0$income_cat <- as.character(hiv0$income_cat)
+hiv0$income_cat[hiv0$income_cat == "1"] <- "< $10,000"
+hiv0$income_cat[hiv0$income_cat == "2"] <- "$10,000 - $40,000"
+hiv0$income_cat[hiv0$income_cat == "3"] <- "$10,000 - $40,000"
+hiv0$income_cat[hiv0$income_cat == "4"] <- "$10,000 - $40,000"
+hiv0$income_cat[hiv0$income_cat == "5"] <- "> $40,000"
+hiv0$income_cat[hiv0$income_cat == "6"] <- "> $40,000"
+hiv0$income_cat[hiv0$income_cat == "7"] <- "> $40,000"
 
+#Education
+table(hiv0$EDUCBAS)
+hiv0$educ_cat <- hiv0$EDUCBAS
+hiv0$educ_cat <- ifelse(hiv0$educ_cat > 3, ">HS", "HS or less")
+table(hiv0$educ_cat)
 
-# Baseline income level - < $10,000, $10,000 - $40,000, >$40,000
-# Education - >HS vs. HS or less
 # ART Adherence, >95% vs. <95%
+table(hiv2$ADH)
+hiv2$adh_cat <- hiv2$ADH
+hiv2$adh_cat <- ifelse(hiv2$adh_cat < 3, ">95%", "<95%")
+table(hiv2$adh_cat)
 
 
+####################
 #Format categorical variables for table
+names(hiv0)
 
-#=============================================================#
-# Explore data- use baseline only (hiv0 set)
-#=============================================================#
-
-names(hiv)
-
-#Primary predictor- hard_drugs
+#hard_drugs
 table(hiv0$hard_drugs)
+hiv0$hard_drugs <- factor(hiv0$hard_drugs, levels = c("0", "1"),
+                          labels = c("No", "Yes"))
 
-#Outcomes- use baseline, then 2 year, then create difference variables
-#Viral load
-summary(hiv0$VLOAD)
-boxplot(hiv0$VLOAD, horizontal = T)
-#Check on plausible numbers--some very high outliers
+#RACE_cat
+table(hiv0$RACE_cat)
+hiv0$RACE_cat <- factor(hiv0$RACE_cat, levels = c("Non-Hispanic White", "Other"))
 
-#CD4
-summary(hiv0$LEU3N)
-boxplot(hiv0$LEU3N, horizontal = T)
+#drink_cat
+table(hiv0$drink_cat)
+hiv0$drink_cat <- factor(hiv0$drink_cat, levels = c("13 or fewer drinks per week", "> 13 drinks per week"))
 
-#AGG_MENT
-summary(hiv0$AGG_MENT)
+#smoke_cat
+table(hiv0$smoke_cat)
+hiv0$smoke_cat <- factor(hiv0$smoke_cat, levels = c("Never/former", "Current"))
 
+#income_cat
+table(hiv0$income_cat)
+hiv0$income_cat <- factor(hiv0$income_cat, levels = c("< $10,000", "$10,000 - $40,000", "> $40,000"))
+
+#educ_cat
+table(hiv0$educ_cat)
+hiv0$educ_cat <- factor(hiv0$educ_cat, levels = c("HS or less", ">HS"))
+
+#adh_cat (at 2 years)
+table(hiv2$adh_cat)
+hiv2$adh_cat <- factor(hiv2$adh_cat, levels = c("<95%", ">95%"))
+
+
+#=============================================================#
+# Merge datasets and create difference variables
+#=============================================================#
+
+####Merge
+names(hiv0)
+names(hiv2)
+
+hiv0_merge <- hiv0[, c(which(colnames(hiv0) == "newid"),
+                       which(colnames(hiv0) == "AGG_MENT"),
+                       which(colnames(hiv0) == "AGG_PHYS"),
+                       which(colnames(hiv0) == "LEU3N"),
+                       which(colnames(hiv0) == "VLOAD"),
+                       which(colnames(hiv0) == "hard_drugs"),
+                       which(colnames(hiv0) == "age"),
+                       which(colnames(hiv0) == "BMI"),
+                       which(colnames(hiv0) == "RACE_cat"),
+                       which(colnames(hiv0) == "drink_cat"),
+                       which(colnames(hiv0) == "smoke_cat"),
+                       which(colnames(hiv0) == "income_cat"),
+                       which(colnames(hiv0) == "educ_cat"))]
+
+colnames(hiv2)[3:7] <- c("AGG_MENT_2yr", "AGG_PHYS_2yr", "LEU3N_2yr",
+                         "VLOAD_2yr", "adh_cat_2yr")
+
+hiv <- merge(hiv0_merge, hiv2, by = "newid")
+
+###########
+# Create difference variables
+
+hiv$diff_vload <- hiv$VLOAD_2yr - hiv$VLOAD
+hiv$diff_leu3n <- hiv$LEU3N_2yr - hiv$LEU3N
+hiv$diff_aggment <- hiv$AGG_MENT_2yr - hiv$AGG_MENT
+hiv$diff_aggphys <- hiv$AGG_PHYS_2yr - hiv$AGG_PHYS
+
+##########
+# Make data set w/ no missing outcomes for each outocme
+vload <- hiv
+summary(vload$diff_vload) #only 19 missing
+vload <- vload[is.na(vload$diff_vload) == FALSE, ]
+  
+leu3n <- hiv
+summary(leu3n$diff_leu3n) #only 19 missing
+leu3n <- leu3n[is.na(leu3n$diff_leu3n) == FALSE, ]
+
+aggment <- hiv
+summary(aggment$diff_aggment) #only 7 missing
+aggment <- aggment[is.na(aggment$diff_aggment) == FALSE, ]
+
+aggphys <- hiv
+summary(aggphys$diff_aggphys)
+aggphys <- aggphys[is.na(aggphys$diff_aggphys) == FALSE, ]
+
+
+#=============================================================#
+# Summarize rest of cont. variables to get rid of NA coding
+#=============================================================#
+
+summary(hiv$BMI)
+hiv$BMI[hiv$BMI == -1] <- NA
+hiv$BMI[hiv$BMI == 999] <- NA
+#still have a maximum of 514.25-- changing to NA for data entry
+hiv$BMI[hiv$BMI > 500] <- NA
+#all seem probable, even if some are fairly low or high
+boxplot(hiv$BMI, horizontal = TRUE)
+
+summary(hiv$age)
+#all seem probable
+
+
+#=============================================================#
+# Create table 1: demographics
+#=============================================================#
+
+#need rows for age (1), bmi (1), hard_drugs (3), race_cat (3),
+  #drink_cat (3), smoke_cat (3), income_cat (4), educ_cat (3),
+  #ADH (3), baseline vload (1), baseline leu3n (1),
+  #baseline aggment (1), baseline aggphys (1)
+num <- 1 + 1 + 3 + 3 + 3 + 3 + 4 + 3 + 3 + 1 + 1 + 1 + 1
+demtab <- matrix(data = NA, nrow = num, ncol = 2)
+
+demtab[1, 1] <- "Age"
+demtab[1, 2] <- paste(round(mean(hiv$age), 2), "±", round(sd(hiv$age), 2))
+
+demtab[2, 1] <- "BMI"
+demtab[2, 2] <- paste(round(mean(hiv$BMI, na.rm = T), 2), "±", round(sd(hiv$BMI, na.rm = T), 2))
+
+demtab[3, 1] <- "Hard drug use at baseline"
+demtab[4:5, 1] <- levels(hiv$hard_drugs)
+demtab[4, 2] <- paste(nrow(hiv[hiv$hard_drugs == "No", ]), paste("(", round(nrow(hiv[hiv$hard_drugs == "No", ])/nrow(hiv) * 100, 2), ")", sep = ""), sep = " ") 
+demtab[5, 2] <- paste(nrow(hiv[hiv$hard_drugs == "Yes", ]), paste("(", round(nrow(hiv[hiv$hard_drugs == "Yes", ])/nrow(hiv) * 100, 2), ")", sep = ""), sep = " ") 
+
+demtab[6, 1] <- "Alcohol use at baseline"
+demtab[7:8, 1] <- levels(hiv$drink_cat)
+demtab[7, 2] <- paste(nrow(hiv[hiv$drink_cat == "13 or fewer drinks per week", ]), 
+                      paste("(", round(nrow(hiv[hiv$drink_cat == "13 or fewer drinks per week", ])/nrow(hiv) * 100, 2), ")", sep = ""), sep = " ") 
+demtab[8, 2] <- paste(nrow(hiv[hiv$drink_cat == "> 13 drinks per week", ]), 
+                      paste("(", round(nrow(hiv[hiv$drink_cat == "> 13 drinks per week", ])/nrow(hiv) * 100, 2), ")", sep = ""), sep = " ") 
+
+demtab[9, 1] <- "Smoking status at baseline"
+demtab[10:11, 1] <- levels(hiv$smoke_cat)
+demtab[10, 2] <- paste(nrow(hiv[hiv$smoke_cat == "Never/former", ]), 
+                      paste("(", round(nrow(hiv[hiv$smoke_cat == "Never/former", ])/nrow(hiv) * 100, 2), ")", sep = ""), sep = " ") 
+demtab[11, 2] <- paste(nrow(hiv[hiv$smoke_cat == "Current", ]), 
+                      paste("(", round(nrow(hiv[hiv$smoke_cat == "Current", ])/nrow(hiv) * 100, 2), ")", sep = ""), sep = " ") 
+
+demtab[12, 1] <- "Income level"
+demtab[13:15, 1] <- levels(hiv$income_cat)
+demtab[13, 2] <- paste(nrow(hiv[hiv$income_cat == "< $10,000", ]), 
+                       paste("(", round(nrow(hiv[hiv$income_cat == "< $10,000", ])/nrow(hiv) * 100, 2), ")", sep = ""), sep = " ") 
+demtab[14, 2] <- paste(nrow(hiv[hiv$income_cat == "$10,000 - $40,000", ]), 
+                       paste("(", round(nrow(hiv[hiv$income_cat == "$10,000 - $40,000", ])/nrow(hiv) * 100, 2), ")", sep = ""), sep = " ") 
+demtab[15, 2] <- paste(nrow(hiv[hiv$income_cat == "> $40,000", ]), 
+                       paste("(", round(nrow(hiv[hiv$income_cat == "> $40,000", ])/nrow(hiv) * 100, 2), ")", sep = ""), sep = " ") 
+#income is wrong- need to deal w/ NAs
+
+#income_cat (4), educ_cat (3),
+#ADH (3), baseline vload (1), baseline leu3n (1),
+#baseline aggment (1), baseline aggphys (1)
+
+#=============================================================#
+# Create table 2: outcomes
+#=============================================================#
